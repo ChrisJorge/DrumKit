4c2368e392415848e6e0dc9ac469a71db32cb67a
Chris <Christopherjorge47@icloud.com>
2024-03-14 19:16:24 -0400

    Created function to play sound when button is clicked

--- app.js
+++ app.js
@@ -2,6 +2,46 @@ for (i = 0; i < document.getElementsByClassName('drum').length; i++)
 {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
         let press = this.innerHTML
-        console.log(press)
+        sound(press)
+        
     });
+}
+
+
+function sound(key)
+{
+    let audio;
+    switch (key)
+    {
+        case 'w':
+            audio = new Audio('./sounds/tom-1.mp3'); audio.play();
+            break;
+
+        case 'a':
+            audio = new Audio('./sounds/tom-2.mp3'); audio.play();
+            break;
+
+        case 's':
+            audio = new Audio('./sounds/tom-3.mp3'); audio.play();
+            break;
+
+        case 'd':
+            audio = new Audio('./sounds/tom-4.mp3'); audio.play();
+            break;
+
+        case 'j':
+            audio = new Audio('./sounds/crash.mp3'); audio.play();
+            break;
+        
+        case 'k':
+            audio = new Audio('./sounds/snare.mp3'); audio.play();
+            break;
+
+        case 'l':
+            audio = new Audio('./sounds/kick-bass.mp3'); audio.play();
+            break;
+        
+        default:
+            break;
+    }
 }
